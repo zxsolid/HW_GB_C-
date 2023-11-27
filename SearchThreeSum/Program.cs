88cf2bfa8390de0e4ee9c5570aa82c64c0494347
zxsolid <stasbaydak@gmail.com>
2023-11-27 20:19:23 +0300

    .

---
+++ SearchThreeSum/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+     * Дан массив и число. Найдите три числа в массиве сумма которых равна искомому числу. 
+     * Подсказка: если взять первое число в массиве, можно ли найти в оставшейся его части два числа равных по сумме первому.
+*/
+
+namespace SearchThreeSum;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 4, 2, 7, 3, 9, 6 };
+        int targetSum = 10;
+
+        numbers.FindThreeNumbers(targetSum).ToList().ForEach(Console.WriteLine);
+    }
+}
