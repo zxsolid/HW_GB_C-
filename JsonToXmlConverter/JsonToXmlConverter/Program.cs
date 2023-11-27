9b9e1978d140a3cac738fcd8a741227773b42aae
zxsolid <stasbaydak@gmail.com>
2023-11-27 20:06:45 +0300

    .

---
+++ JsonToXmlConverter/JsonToXmlConverter/Program.cs
@@ -0,0 +1,25 @@
+﻿using JsonXmlLib;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            string jsonInput = File.ReadAllText("input.json");
+
+            string xmlOutput = jsonInput.ConvertJsonToXml();
+
+            File.WriteAllText("output.xml", xmlOutput);
+
+            Console.WriteLine("Конвертация JSON в XML завершена.");
+            Console.WriteLine(xmlOutput);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+
+        Console.ReadLine();
+    }
+}
---
+++ JsonToXmlConverter/JsonXmlLib/JsonXml.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System.Xml.Linq;
+using System.Xml;
+
+namespace JsonXmlLib
+{
+    public static class JsonXml
+    {
+        public static string ConvertJsonToXml(this string json)
+        {
+            try
+            {
+                XmlDocument? xmlDoc = JsonConvert.DeserializeXmlNode(json, "root", true);
+                XDocument xDoc = XDocument.Parse(xmlDoc.OuterXml);
+                return xDoc.ToString();
+            }
+            catch 
+            {
+                return string.Empty;
+            }
+           
+        }
+    }
+}
